2c8c67dec02b3799e5b6fe9b90ef5f6a5cfeee8c
Insektosaurus <Mark.Korjagin@gmail.com>
2019-03-02 14:55:11 +0100

    Add project files.

---
+++ JamminsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JamminsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
